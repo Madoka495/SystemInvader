81edc596d0b53795812c225507ec968e879127c2
Jotaro <cislonche20@hotmail.fr>
2016-11-16 09:43:18 +0100

    projectiles & tours

--- SystemInvader/Projectile.cs
+++ SystemInvader/Projectile.cs
@@ -12,7 +12,11 @@ namespace SystemInvader
         Vector2 _current = new Vector2();
         Vector2 _dest = new Vector2();
         Vector2 _start = new Vector2();
+        Vector2 _diff = new Vector2();
         int _speed;
+        bool _xPos;
+        bool _yPos;
+        bool _reached;
 
         public Projectile(Vector2 start, Vector2 dest, int speed)
         {
@@ -24,34 +28,149 @@ namespace SystemInvader
 
         public void Update()
         {
-            if(_current.X < _dest.X)
+            if(_start.X < _dest.X)
             {
-                _current.X += (_dest.X - _start.X) / 100 * _speed;
+                _xPos = true;
+                _diff.X = _dest.X - _start.X;
             }
-            else if(_current.X > _dest.X)
+            else if(_start.X > _dest.X)
             {
-                _current.X -= (_start.X - _dest.X) / 100 * _speed;
+                _xPos = false;
+                _diff.X = _start.X - _dest.X;
+            }
+            
+            if (_start.Y < _dest.Y)
+            {
+                _yPos = true;
+                _diff.Y = _dest.Y - _start.Y;
+            }
+            else if (_start.Y > _dest.Y)
+            {
+                _yPos = false;
+                _diff.Y = _start.Y - _dest.Y;
             }
 
-            if (_current.Y < _dest.Y)
+            if(Math.Abs(_diff.X) == Math.Abs(_diff.Y))
             {
-                _current.Y += (_dest.Y - _start.Y) / 100 * _speed;
-            }
-            else if (_current.Y > _dest.Y)
-            {
-                _current.Y -= (_start.Y - _dest.Y) / 100 * _speed;
-            }
-        }
+                if(_xPos == true)
+                {
+                    _current.X += _speed;
+                }
+                else if(_xPos == false)
+                {
+                    _current.X -= _speed;
+                }
 
-        public bool DestReached()
-        {
-            if (_current.X == _dest.X && _current.Y == _dest.Y)
+                if (_yPos == true)
+                {
+                    _current.Y += _speed;
+                }
+                else if (_yPos == false)
+                {
+                    _current.Y -= _speed;
+                }
+            }
+            else if(Math.Abs(_diff.X) > Math.Abs(_diff.Y))
             {
-                return true;
+                if (_xPos == true)
+                {
+                    _current.X += _speed;
+                }
+                else if (_xPos == false)
+                {
+                    _current.X -= _speed;
+                }
+
+                if (_yPos == true)
+                {
+                    _current.Y += Math.Abs(_diff.Y) / Math.Abs(_diff.X) * _speed;
+                }
+                else if (_yPos == false)
+                {
+                    _current.Y -= Math.Abs(_diff.Y) / Math.Abs(_diff.X) * _speed;
+                }
+            }
+            else if (Math.Abs(_diff.X) < Math.Abs(_diff.Y))
+            {
+                if (_xPos == true)
+                {
+                    _current.X += Math.Abs(_diff.X) / Math.Abs(_diff.Y) * _speed;
+                }
+                else if (_xPos == false)
+                {
+                    _current.X -= Math.Abs(_diff.X) / Math.Abs(_diff.Y) * _speed;
+                }
+
+                if (_yPos == true)
+                {
+                    _current.Y += _speed;
+                }
+                else if (_yPos == false)
+                {
+                    _current.Y -= _speed;
+                }
+            }
+            if (_start.X < _dest.X)
+            {
+                if (_start.Y < _dest.Y)
+                {
+                    if (_current.X >= _dest.X && _current.Y >= _dest.Y)
+                    {
+                        _reached = true;
+                    }
+                    else
+                    {
+                        _reached = false;
+                    }
+                }
+                else
+                {
+                    if (_current.X >= _dest.X && _current.Y <= _dest.Y)
+                    {
+                        _reached = true;
+                    }
+                    else
+                    {
+                        _reached = false;
+                    }
+                }
             }
             else
             {
-                return false;
+                if (_start.Y < _dest.Y)
+                {
+                    if (_current.X <= _dest.X && _current.Y >= _dest.Y)
+                    {
+                        _reached = true;
+                    }
+                    else
+                    {
+                        _reached = false;
+                    }
+                }
+                else
+                {
+                    if (_current.X <= _dest.X && _current.Y <= _dest.Y)
+                    {
+                        _reached = true;
+                    }
+                    else
+                    {
+                        _reached = false;
+                    }
+                }
+            }
+        }
+
+        public bool DestReached
+        {
+            get
+            {
+                return _reached;
+            }
+            set
+            {
+                _reached = value;
             }
         }
 
--- SystemInvader/Tower.cs
+++ SystemInvader/Tower.cs
@@ -1,8 +1,57 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SystemInvader;
 
-public class Class1
+public class Tower
 {
-	public Class1()
+    Vector2 _position = new Vector2();
+    int _rate;
+    int _range;
+    int _type;
+    List<Projectile> _projectiles;
+
+    public Tower(Vector2 position, int rate, int range, int type)
 	{
-	}
+        _position = position;
+        _rate = rate;
+        _range = range;
+        _type = type;
+        _projectiles = new List<Projectile>();
+    }
+
+    public void Shoot(Vector2 enemyPosition)
+    {
+        if (_type == 1)
+        {
+            _projectiles.Add(new Projectile(_position, enemyPosition, 6));
+        }
+        else if (_type == 2)
+        {
+            _projectiles.Add(new Projectile(_position, enemyPosition, 15));
+        }
+    }
+
+    public Vector2 GetPos()
+    {
+        return _position;
+    }
+
+    public int GetRange()
+    {
+        return _range;
+    }
+
+    public int GetRate()
+    {
+        return _rate;
+    }
+
+    public List<Projectile> GetProjectiles()
+    {
+        return _projectiles;
+    }
 }
