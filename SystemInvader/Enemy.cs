f19af0f9a63b24712b4fc1440adab0d7ef5112a4
torung <trung.lespace@gmail.com>
2016-11-16 10:13:11 +0100

    Enemy, level, Game1 provisoire

--- SystemInvader/Enemy.cs
+++ SystemInvader/Enemy.cs
@@ -3,10 +3,83 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
-namespace SystemInvader
+namespace VirusInvader
 {
-    class Enemy
+    public class Enemy
     {
+        protected int _Health;
+        protected int _currentHealth;
+
+        protected bool _alive = true;
+
+        protected float _speed;
+        protected int _price;
+
+        protected Texture2D _texture;
+
+        protected Vector2 _position;
+        protected Vector2 _velocity;
+
+        public Enemy(Texture2D texture, Vector2 position, int health, int bountyGiven, float speed)
+        {
+            _texture = texture;
+            _position = position;
+
+            _Health = health;
+            _currentHealth = _Health;
+            _price = bountyGiven;
+            _speed = speed;
+        }
+
+        //public bool IsDead => _currentHealth <= 0;
+        //public int BountyGiven => _price;
+        
+        private Queue<Vector2> _waypoints = new Queue<Vector2>();
+
+        public void SetWaypoints(Queue<Vector2> waypointsRefer)
+        {
+            foreach (Vector2 waypoint in waypointsRefer)
+                _waypoints.Enqueue(waypoint);
+
+            _position = _waypoints.Dequeue();
+        }
+
+        public float DistanceToDestination => Vector2.Distance(_position, _waypoints.Peek());
+
+        public void Update(GameTime gameTime)
+        {
+            if (_waypoints.Count > 0)
+            {
+                if (DistanceToDestination < _speed)
+                {
+                    _position = _waypoints.Peek();
+                    _waypoints.Dequeue();
+                }
+
+                else
+                {
+                    Vector2 direction = _waypoints.Peek() - _position;
+                    direction.Normalize();
+
+                    _velocity = Vector2.Multiply(direction, _speed);
+
+                    _position += _velocity;
+                }
+            }
+            else
+                _alive = false;
+
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            if(_alive)
+                spriteBatch.Draw(_texture, _position, Color.White);
+        }
+
     }
 }
---
+++ SystemInvader/Level.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace VirusInvader
+{
+    public class Level
+    {
+        int[,] map = new int[,]
+        {
+            {0,0,1,0,0,0,0,0,},
+            {0,0,1,1,0,0,0,0,},
+            {0,0,0,1,1,0,0,0,},
+            {0,0,0,0,1,0,0,0,},
+            {0,0,0,1,1,0,0,0,},
+            {0,0,1,1,0,0,0,0,},
+            {0,0,1,0,0,0,0,0,},
+            {0,0,1,1,1,1,1,1,},
+        };
+        //chung ta se tao mot lop QUEUE de type Vector2
+        private Queue<Vector2> waypoints = new Queue<Vector2>();
+        //Sau do them vao elements
+        //QUEUE   ->=====-> first in first out
+        public Level()
+        {
+            waypoints.Enqueue(new Vector2(2, 0) * 32);//start
+            waypoints.Enqueue(new Vector2(2, 1) * 32);
+            waypoints.Enqueue(new Vector2(3, 1) * 32);
+            waypoints.Enqueue(new Vector2(3, 2) * 32);
+            waypoints.Enqueue(new Vector2(4, 2) * 32);
+            waypoints.Enqueue(new Vector2(4, 4) * 32);
+            waypoints.Enqueue(new Vector2(3, 4) * 32);
+            waypoints.Enqueue(new Vector2(3, 5) * 32);
+            waypoints.Enqueue(new Vector2(2, 5) * 32);
+            waypoints.Enqueue(new Vector2(2, 7) * 32);
+            waypoints.Enqueue(new Vector2(7, 7) * 32);//end
+            
+        }
+        public Queue<Vector2> Waypoints => waypoints;
+
+        private List<Texture2D> TileTexture = new List<Texture2D>();
+        
+        
+
+        public void AddTexture(Texture2D texture)
+        {
+            TileTexture.Add(texture);
+        }
+
+        public int Width => map.GetLength(1);
+        public int Height => map.GetLength(0);
+
+        public void Draw(SpriteBatch batch)
+        {
+            for (int i = 0; i < Width; i++)
+            {
+                for (int j = 0; j < Height; j++)
+                {
+                    int textureIndex = map[j, i];
+                    Texture2D texture = TileTexture[textureIndex];
+
+                    batch.Draw(texture, new Rectangle(i * 32, j * 32, 32, 32), Color.White);
+                }
+            }
+        }
+
+
+
+
+    }
+}
