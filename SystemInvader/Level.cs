0a5624c93650950d46df60aa414907a051b3dc08
torung <trung.lespace@gmail.com>
2016-11-17 09:44:27 +0100

    Level

--- SystemInvader/Level.cs
+++ SystemInvader/Level.cs
@@ -14,21 +14,22 @@ namespace VirusInvader
     {
         int[,] map = new int[,]
         {
-            {0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-            {0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-            {0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-            {0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-            {0,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-            {0,1,0,0,1,0,0,0,0,1,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0},
-            {0,1,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0},
-            {0,1,1,1,1,0,0,0,0,1,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0},
-            {0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,1,0,0,0,0,0,0},
-            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0},
-            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0},
-            {0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,1,0,1,0,0,0,0,1,0},
-            {0,0,0,0,1,1,0,0,0,0,0,0,1,0,0,0,1,1,1,0,0,0,0,1,0},
-            {0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0},
-            {0,0,0,0,1,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,0}
+           //0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4
+            {0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},//0
+            {0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},//1
+            {0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},//2
+            {0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},//3
+            {0,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},//4
+            {0,1,0,0,1,0,0,0,0,1,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0},//5
+            {0,1,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0},//6
+            {0,1,1,1,1,0,0,0,0,1,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0},//7
+            {0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,1,0,0,0,0,0,0},//8
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0},//9
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0},//10
+            {0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,1,0,1,0,0,0,0,1,0},//11
+            {0,0,0,0,1,1,0,0,0,0,0,0,1,0,0,0,1,1,1,0,0,0,0,1,0},//12
+            {0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0},//12
+            {0,0,0,0,1,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,0} //13
         };
         //chung ta se tao mot lop QUEUE de type Vector2
         private Queue<Vector2> waypoints = new Queue<Vector2>();
@@ -36,18 +37,29 @@ namespace VirusInvader
         //QUEUE   ->=====-> first in first out
         public Level()
         {
-            waypoints.Enqueue(new Vector2(2, 0) * 32);//start
-            waypoints.Enqueue(new Vector2(2, 1) * 32);
-            waypoints.Enqueue(new Vector2(3, 1) * 32);
-            waypoints.Enqueue(new Vector2(3, 2) * 32);
+            waypoints.Enqueue(new Vector2(2, 0) * 32);
+            waypoints.Enqueue(new Vector2(2, 2) * 32);
             waypoints.Enqueue(new Vector2(4, 2) * 32);
-            waypoints.Enqueue(new Vector2(4, 4) * 32);
-            waypoints.Enqueue(new Vector2(3, 4) * 32);
-            waypoints.Enqueue(new Vector2(3, 5) * 32);
-            waypoints.Enqueue(new Vector2(2, 5) * 32);
-            waypoints.Enqueue(new Vector2(2, 7) * 32);
-            waypoints.Enqueue(new Vector2(7, 7) * 32);//end
-            
+            waypoints.Enqueue(new Vector2(4, 7) * 32);
+            waypoints.Enqueue(new Vector2(1, 7) * 32);
+            waypoints.Enqueue(new Vector2(1, 4) * 32);
+            waypoints.Enqueue(new Vector2(9, 4) * 32);
+            waypoints.Enqueue(new Vector2(9, 8) * 32);
+            waypoints.Enqueue(new Vector2(14, 8) * 32);
+            waypoints.Enqueue(new Vector2(14, 5) * 32);
+            waypoints.Enqueue(new Vector2(18, 5) * 32);
+            waypoints.Enqueue(new Vector2(18, 12) * 32);
+            waypoints.Enqueue(new Vector2(16, 12) * 32);
+            waypoints.Enqueue(new Vector2(16, 10) * 32);
+            waypoints.Enqueue(new Vector2(23, 10) * 32);
+            waypoints.Enqueue(new Vector2(23, 14) * 32);
+            waypoints.Enqueue(new Vector2(12, 14) * 32);
+            waypoints.Enqueue(new Vector2(12, 11) * 32);
+            waypoints.Enqueue(new Vector2(5, 11) * 32);
+            waypoints.Enqueue(new Vector2(5, 12) * 32);
+            waypoints.Enqueue(new Vector2(4, 12) * 32);
+            waypoints.Enqueue(new Vector2(4, 12) * 32);
+
         }
         public Queue<Vector2> Waypoints => waypoints;
 
